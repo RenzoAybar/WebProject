51f9fb0dc0482cedd5cb42a5277cbe3a063fc501
JeanpieroS <135179957+KairoMN@users.noreply.github.com>
2025-06-12 21:44:46 -0500

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,10 @@ import { HomePage } from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
 import { CheckoutPage } from '../pages/CheckoutPage'; // Importar CheckoutPage
+import { PantalonesPage } from '../pages/PantalonesPage';
+import { VestidosPage } from '../pages/VestidosPage';
+import { PolosPage } from '../pages/PolosPage';
+import { ZapatillasPage } from '../pages/ZapatillasPage';
 
 export const router = createBrowserRouter([
     {
@@ -25,6 +29,22 @@ export const router = createBrowserRouter([
             {
                 path: 'checkout', // Nueva ruta
                 element: <CheckoutPage />,
+            },
+            {
+                path: '/pantalones',
+                element: <PantalonesPage />
+            },
+            {
+                path: '/vestidos',
+                element: <VestidosPage />
+            },
+            {
+                path: '/polos',
+                element: <PolosPage />
+            },
+            {
+                path: '/zapatillas',
+                element: <ZapatillasPage />
             },
 
         ]
